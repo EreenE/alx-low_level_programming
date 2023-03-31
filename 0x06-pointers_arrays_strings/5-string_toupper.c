c9c20bff02f29db4b82f5798fb192b30b1e3992c
EreenE <EreenE.gmail.com>
2023-03-30 18:17:05 -0700

    a

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,38 +1,21 @@
 #include "main.h"
-
 /**
-
- * string_toupper - change all lowercase to uppercase
-
- * @n: pointer
-
- *
-
- * Return: n
-
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
  */
-
-char *string_toupper(char *n)
-
+char *string_toupper(char *s)
 {
 
-        int i;
+	int i;
 
-
-        i = 0;
-
-        while (n[i] != '\0')
-
-        {
-
-                if (n[i] >= 'a' && n[i] <= 'z')
-
-                        n[i] = n[i] - 32;
-
-                i++;
-
-        }
-
-        return (n);
-
+i = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
+	}
+	return (s);
 }
+/*Bwave ICT */
